987e732a62c02ba6c40fa3e602b548dd52d8ac29
saisudheer123 <41769759+saisudheer123@users.noreply.github.com>
2018-08-02 11:00:10 +0100

    Create saivowel.py

---
+++ saivowel.py
@@ -0,0 +1,8 @@
+inp=input()
+if(inp>='a' and inp<='z') or (inp>='A' and inp<='Z'):
+    if inp in ['a','e','i','o','u','A','E','I','O','U']:
+        print("Vowel")
+    else:
+        print("Consonant")
+else:
+    print("invalid")
